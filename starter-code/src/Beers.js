4d5b526d56c798904cbde7b2b41d843aeac69adf
jnguyen3436 <jnguyen3436@gmail.com>
2019-11-18 12:06:45 -0500

    added rndom

--- starter-code/src/Beers.js
+++ starter-code/src/Beers.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
 import axios from 'axios'
+import {Link} from "react-router-dom"
 
 export default class Beers extends Component {
 
     state = {allTheBeers:[]}
 
-
+        
         getAllBeers =()=>{
         axios.get("https://ih-beers-api2.herokuapp.com/beers",).then(allTheBeers=>{
           console.log(allTheBeers)
@@ -15,11 +16,13 @@ export default class Beers extends Component {
       showTheBeers=()=>{
         return this.state.allTheBeers.map(eachBeer=>{
             return(
+                <Link to={eachBeer._id}>
                 <li>
                 <h4>{eachBeer.name}</h4>
                 <img src={eachBeer.image_url} width="200px" alt={eachBeer.name}/>
                 <p>{eachBeer.description}</p>
                 </li>
+                </Link>
             )
         })
 
--- starter-code/src/RandomBeer.js
+++ starter-code/src/RandomBeer.js
@@ -1,10 +1,33 @@
 import React, { Component } from 'react'
+import axios from "axios"
 
 export default class RandomBeer extends Component {
+state ={
+    allRandom: []
+}
+    componentDidMount(){
+        axios.get("https://ih-beers-api2.herokuapp.com/beers/random",).then(allRandom=>{
+          console.log(allRandom)
+          this.setState(
+              {allRandom:allRandom.data}
+          )
+        })
+      }
+    // showRandomBeer =()=>{
+    //     return (
+    //         // <li>
+    //         //     {this.allRandom.name}
+    //         //     {this.allRandom.image_url}
+    //         //     {this.allRandom.description}
+    //         // </li>
+    //     )
+    // }  
     render() {
         return (
             <div>
+                
                 Random
+                
             </div>
         )
     }
