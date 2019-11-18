74cf7fbe62d05a3b22ff0c87e707318a2cbf3f3b
jnguyen3436 <jnguyen3436@gmail.com>
2019-11-18 14:44:15 -0500

    i'm tired of this lab :(

--- starter-code/src/RandomBeer.js
+++ starter-code/src/RandomBeer.js
@@ -1,40 +1,47 @@
 import React, { Component } from 'react'
-import axios from "axios"
+import axios from 'axios'
 
-export default class RandomBeer extends Component {
-state ={
-    allRandom: []
-}
-    componentDidMount(){
-        axios.get("https://ih-beers-api2.herokuapp.com/beers/random",).then(allRandom=>{
-          console.log(allRandom)
-          this.setState(
-              {allRandom:allRandom.data}
-          )
-        })
-      }
-    // showRandomBeer =()=>{
-    //     return (
-    //         // <li>
-    //         //     {this.allRandom.name}
-    //         //     {this.allRandom.image_url}
-    //         //     {this.allRandom.description}
-    //         // </li>
-    //     )
-    // }  
-    render() {
-        return (
-            <div>
-                 <li>
-                    
-                {this.state.allRandom.name}
-                
-                 <img src = {this.state.allRandom.image_url} alt="askldfnasdf"/>
-                  {this.state.allRandom.description}
-                 </li>
-                Random
-                
+export default class RandomBeers extends Component {
+  state = {
+    beer: {}
+  }
+
+  componentDidMount( ) {
+    axios.get('https://api.punkapi.com/v2/beers/random').then(data => {
+      this.setState({
+        beer: data.data[0]
+      })
+    })
+  }
+
+
+
+
+
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+        <div className="col-xs-12 mx-auto">
+        <img src={this.state.beer.image_url} alt="" className="beer-image2" />
+        <div className="beer-row">
+          <div>
+              <h1>{this.state.beer.name}</h1>
+              <h2>{this.state.beer.tagline}</h2>
             </div>
-        )
-    }
+            <div>
+              <p>{this.state.beer.first_brewed}</p>
+              <p>{this.state.beer.attenuation_level}</p>
+            </div>
+          </div>
+
+            <div>
+              <p>{this.state.beer.description}</p>
+              <sub>{this.state.beer.contributed_by}</sub>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
